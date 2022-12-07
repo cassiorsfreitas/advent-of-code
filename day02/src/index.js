a5b1fa35fa69f5673f5f5d9df66d137ef08d4d8a
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2022-12-07 23:51:41 +0000

    feat: solve part two from day02

--- day02/src/index.js
+++ day02/src/index.js
@@ -2,15 +2,15 @@ import fs from 'fs/promises'
 const PATH = './mock/prod.txt'
 
 const ENUM_MAP_SCORE = {
-	'A X': 4, // 1 + 3
-	'A Y': 8, // 2 + 6
-	'A Z': 3, // 3 + 0
+	'A X': 3, // 3 + 0
+	'A Y': 4, // 1 + 3
+	'A Z': 8, // 2 + 6
 	'B X': 1, // 1 + 0
 	'B Y': 5, // 2 + 3
 	'B Z': 9, // 3 + 6
-	'C X': 7, // 1 + 6
-	'C Y': 2, // 2 + 0
-	'C Z': 6, // 3 + 3
+	'C X': 2, // 2 + 0
+	'C Y': 6, // 3 + 3
+	'C Z': 7, // 1 + 6
 }
 
 const getLineScore = (line) => {
